6af1f00889d97e6aed563d241f6a368a5d7a61e9
Ben <nvbien@Nguyens-MacBook-Pro-2.local>
2025-04-28 15:02:13 +0700

    load config

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,15 +1,28 @@
 package main
 
 import (
+	"fmt"
+	"log"
+
 	"github.com/gin-gonic/gin"
+	"github.com/nvbien2000/portfolio_backend/internal/config"
 	"github.com/nvbien2000/portfolio_backend/internal/handler/http"
 )
 
 func main() {
+	cfg, err := config.LoadConfig()
+	if err != nil {
+		log.Fatalf("Failed to load config: %v", err)
+	}
 
 	router := gin.Default()
 
 	http.SetUpRoutes(router)
 
-	router.Run(":8080")
+	serverAddress := fmt.Sprintf(":%s", cfg.ServerPort)
+	log.Printf("Starting server on %s", serverAddress)
+
+	if err := router.Run(serverAddress); err != nil {
+		log.Fatalf("Failed to start server: %v", err)
+	}
 }
--- internal/config/config.go
+++ internal/config/config.go
@@ -1,1 +1,32 @@
 package config
+
+import (
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+type (
+	// Config is the application configuration
+	Config struct {
+		ServerPort string
+	}
+)
+
+// LoadConfig loads the application configuration from the .env file
+func LoadConfig() (config *Config, err error) {
+	if err = godotenv.Load(os.ExpandEnv(".env")); err != nil {
+		return nil, err
+	}
+
+	serverPort := os.Getenv("SERVER_PORT")
+	if serverPort == "" {
+		serverPort = "8080"
+	}
+
+	config = &Config{
+		ServerPort: serverPort,
+	}
+
+	return config, nil
+}
